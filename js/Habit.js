2bc06cd5d4fc37f22bb288a96800b768da69113a
Ola <olafrankowska112@gmail.com>
2019-11-04 18:47:20 +0100

    restyled the app

--- js/Habit.js
+++ js/Habit.js
@@ -11,17 +11,17 @@ class Habit {
         habitContainer.innerHTML += 
         `
         <div id=${this.id} class="showHabit">
-            <h4>${this.name}</h4>
-            <div class="d-flex">
+            <h4 class="text-white">${this.name}</h4>
+            <div class="d-flex ">
                 <div class="progress">
                     <div id="${this.color}" class="progress-bar bg-${this.color}" role="progressbar" style="width: ${this.complete/this.goal * 100}%" aria-valuenow="25"
                         aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <button type="button" class="btn btn-secondary progress-btn"><i class="fas fa-chevron-right"></i></button>
-                <button type="button" class="btn btn-secondary delete-btn"><i class="fas fa-times"></i></button>
+                <button type="button" class="btn btn-dark progress-btn"><i class="fas fa-chevron-right"></i></button>
+                <button type="button" class="btn btn-dark delete-btn"><i class="fas fa-times"></i></button>
         
             </div>
-            <p class="text-muted"><span class ="complete">${this.complete}</span> / <span class="goal">${this.goal}</span></p>
+            <p class="text-white"><span class ="complete">${this.complete}</span> / <span class="goal">${this.goal}</span></p>
         </div>
         `;
     }
